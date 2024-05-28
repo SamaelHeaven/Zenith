77f68de66891b961c9b2b0ec47990ce538862173
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-28 01:41:29 -0400

    Feat: Core features

--- src/main/kotlin/org/samaelheaven/zenith/core/Renderer.kt
+++ src/main/kotlin/org/samaelheaven/zenith/core/Renderer.kt
@@ -2,7 +2,6 @@ package org.samaelheaven.zenith.core
 
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
-import javafx.scene.paint.Paint
 
 class Renderer private constructor() {
     private lateinit var graphicsContext: GraphicsContext
@@ -11,11 +10,6 @@ class Renderer private constructor() {
         private var instance: Renderer? = null
         private val renderer: Renderer
             get() = (if (instance == null) Renderer() else instance).also { instance = it }!!
-
-        fun clearBackground(paint: Paint) {
-            renderer.graphicsContext.fill = paint
-            renderer.graphicsContext.fillRect(0.0, 0.0, Game.width.toDouble(), Game.height.toDouble())
-        }
 
         internal fun initialize(fxCanvas: Canvas) {
             renderer.graphicsContext = fxCanvas.graphicsContext2D

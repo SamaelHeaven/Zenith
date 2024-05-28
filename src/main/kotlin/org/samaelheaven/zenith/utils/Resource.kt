3a1b083b5f1bc0842c30bf0577c33954e496fe16
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-28 17:34:33 -0400

    Added texture function

--- src/main/kotlin/org/samaelheaven/zenith/utils/Resource.kt
+++ src/main/kotlin/org/samaelheaven/zenith/utils/Resource.kt
@@ -1,7 +1,10 @@
 package org.samaelheaven.zenith.utils
 
+import org.samaelheaven.zenith.asset.Texture
 import java.io.InputStream
 import java.net.URL
+
+private val textures = HashMap<String, Texture>()
 
 fun url(path: String): URL {
     return ClassLoader.getSystemResource(path)!!
@@ -9,6 +12,10 @@ fun url(path: String): URL {
 
 fun stream(path: String): InputStream {
     return ClassLoader.getSystemResourceAsStream(path)!!
+}
+
+fun texture(path: String): Texture {
+    return textures.getOrPut(path) { Texture(url(path)) }
 }
 
 fun path(path: String): String {

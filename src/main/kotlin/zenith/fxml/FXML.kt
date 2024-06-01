3e36926097220b6b2ed0b1a3fb871e5e814cc5e6
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-31 23:14:56 -0400

    Feat: You can load fxml form a stream

--- src/main/kotlin/zenith/fxml/FXML.kt
+++ src/main/kotlin/zenith/fxml/FXML.kt
@@ -2,6 +2,7 @@ package zenith.fxml
 
 import javafx.fxml.FXMLLoader
 import zenith.io.Resource
+import java.io.InputStream
 import java.net.URL
 
 object FXML {
@@ -12,5 +13,10 @@ object FXML {
     fun <T> load(url: URL): T {
         val fxmlLoader = FXMLLoader(url)
         return fxmlLoader.load()
+    }
+
+    fun <T> load(input: InputStream): T {
+        val fxmlLoader = FXMLLoader()
+        return fxmlLoader.load(input)
     }
 }

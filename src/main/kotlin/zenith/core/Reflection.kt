5dbe79f540dd0830416623cbaa88310a1e532548
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-05-31 23:29:36 -0400

    Test

--- src/main/kotlin/zenith/core/Reflection.kt
+++ src/main/kotlin/zenith/core/Reflection.kt
@@ -2,10 +2,7 @@ package zenith.core
 
 internal object Reflection {
     fun getCallerClass(level: Int): Class<*> {
-        val stElements = Thread.currentThread().stackTrace
-        val rawFQN = stElements[level + 1].toString().split("\\(".toRegex()).dropLastWhile { it.isEmpty() }
-            .toTypedArray()[0]
-        println(rawFQN.substring(0, rawFQN.lastIndexOf('.')))
-        return Class.forName(rawFQN.substring(0, rawFQN.lastIndexOf('.')))
+        println(Thread.currentThread().stackTrace[level].className)
+        return Class.forName(Thread.currentThread().stackTrace[level].className)
     }
 }

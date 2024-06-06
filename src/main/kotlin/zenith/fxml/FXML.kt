b7198e1ce757df61e9c4af784fa90702821cd7d2
SamaelHeaven <samuelparadisinfo@gmail.com>
2024-06-06 12:55:41 -0400

    Fix: Minor fixes

--- src/main/kotlin/zenith/fxml/FXML.kt
+++ src/main/kotlin/zenith/fxml/FXML.kt
@@ -6,20 +6,20 @@ import java.io.InputStream
 import java.net.URL
 
 object FXML {
-    private val fxmlLoader = FXMLLoader()
-
-    fun <T> load(resource: String): T {
-        return load(Resource.url(resource))
+    fun <T> load(resource: String, controller: Any? = null): T {
+        return load(Resource.url(resource), controller)
     }
 
-    fun <T> load(url: URL): T {
+    fun <T> load(url: URL, controller: Any? = null): T {
         url.openStream().use {
-            return load(it)
+            return load(it, controller)
         }
     }
 
-    fun <T> load(input: InputStream): T {
+    fun <T> load(input: InputStream, controller: Any? = null): T {
+        val fxmlLoader = FXMLLoader()
         fxmlLoader.setRoot(null)
+        controller?.let { fxmlLoader.setController(controller) }
         return fxmlLoader.load(input)
     }
 }

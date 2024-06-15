dc6f4dd8b7918497b66b747a3e778e4bc21b2154
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-06-15 18:42:19 -0400

    Refactor: Minor changes

--- src/main/kotlin/zenith/core/Component.kt
+++ src/main/kotlin/zenith/core/Component.kt
@@ -9,7 +9,6 @@ abstract class Component {
     protected open val originProperty get() = entity!!.originProperty
     protected open val pivotPointProperty get() = entity!!.pivotPointProperty
     protected open val rotationProperty get() = entity!!.rotationProperty
-    protected open val boundingBox get() = entity!!.boundingBox
     val entity: Entity? get() = _entity
 
     protected open var position: Vector2
@@ -40,6 +39,12 @@ abstract class Component {
         get() = entity!!.rotation
         set(value) {
             entity!!.rotation = value
+        }
+
+    protected open var boundingBox
+        get() = entity!!.boundingBox
+        set(value) {
+            entity!!.boundingBox = value
         }
 
     protected open fun start() {}

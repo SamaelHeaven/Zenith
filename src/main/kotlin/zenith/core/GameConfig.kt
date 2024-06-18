fb54ae5ff81d9ed19f6152b38e2e12d72ab4a8f6
Samuel Paradis <samuelparadisinfo@gmail.com>
2024-06-18 17:13:11 -0400

    Feat: Default refresh rate is now the one of the display

--- src/main/kotlin/zenith/core/GameConfig.kt
+++ src/main/kotlin/zenith/core/GameConfig.kt
@@ -1,12 +1,13 @@
 package zenith.core
 
 import javafx.beans.NamedArg
+import java.awt.GraphicsEnvironment
 import java.net.URL
 
 data class GameConfig(
     @NamedArg("width") val width: Int = 800,
     @NamedArg("height") val height: Int = 600,
-    @NamedArg("fpsTarget") val fpsTarget: Int = 60,
+    @NamedArg("fpsTarget") val fpsTarget: Int = GraphicsEnvironment.getLocalGraphicsEnvironment().screenDevices[0].getDisplayMode().refreshRate,
     @NamedArg("title") val title: String = "Game",
     @NamedArg("fullscreen") val fullscreen: Boolean = false,
     @NamedArg("decorated") val decorated: Boolean = true,
